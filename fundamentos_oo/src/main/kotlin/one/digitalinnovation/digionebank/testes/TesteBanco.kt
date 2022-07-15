6ea0cf6527ebe071a0ad3ec9f75a561fda5614f4
suellenmuniz <su09muniz@hotmail.com>
2022-07-14 21:58:14 -0300

    updated bank test class

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteBanco.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteBanco.kt
@@ -3,12 +3,12 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Banco
 
 fun main() {
-    val digiOneBank = Banco(nome = "DigiOne", numero = 12)
+    val dig1OneBank = Banco(nome = "Dig1One", numero = 12)
 
-    println(digiOneBank.nome)
-    println(digiOneBank.numero)
+    println(dig1OneBank.nome)
+    println(dig1OneBank.numero)
 
-    val banco2 = digiOneBank.copy(nome = "Banco2")
+    val banco2 = dig1OneBank.copy(nome = "banco2")
 
     println(banco2.info())
 
